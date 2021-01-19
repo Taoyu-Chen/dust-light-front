55ee83163157eb0aa5c24d1e2533509e857401cf
SevenSound <875693150@qq.com>
2021-01-19 15:13:12 +0800

    import ant-design-vue

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 import 'normalize.css'
 import './style/base.scss'
-
-createApp(App).use(store).use(router).mount('#app')
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+createApp(App, Antd).use(store).use(router).use(Antd).mount('#app')
