0e3138ab4f2f8e6e9fde93c1af3dc8cd603ad546
SevenSound <875693150@qq.com>
2021-02-22 20:34:58 +0800

    complete header

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,13 @@
     <router-link to="/publish/task">Publish Task</router-link>
   </div>
   <router-view/> -->
+  <div class="wrapper">
+    <div class="header">
+      <span class="iconfont header__icon">&#xe646;</span>
+      Welcome to Dust Light App
+      <span class="iconfont header__notice">&#xe609;</span>
+    </div>
+  </div>
   <div class="docker">
     <span class="docker__item docker__item--active">
       <div class="iconfont">&#xe6ac;</div>
@@ -26,6 +33,41 @@
 </template>
 
 <style lang="scss">
+@import './style/variables.scss';
+@import './style/mixins.scss';
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  background: $wrapper-fontcolor;
+  padding: 0 .18rem;
+}
+
+.header {
+  position: relative;
+  padding: .16rem 0;
+  line-height: .22rem;
+  font-size: .21rem;
+  @include ellipsis;
+  .header__icon {
+    position: relative;
+    margin-right: .24rem;
+    right: .05rem;
+    top: .01rem;
+    margin: .08rem;
+    font-size: .2rem;
+  }
+  .header__notice {
+    position: relative;
+    left: .05rem;
+    top: .01rem;
+    margin: .08rem;
+    font-size: .2rem;
+  }
+}
+
 .docker{
   display: flex;
   position: absolute;
@@ -35,7 +77,7 @@
   width: 100%;
   height: .53rem;
   border-top: 1px solid #F1F1F1;
-  background-color: orange;
+  background-color: $docker-fontcolor;
   &__item {
     flex: 1;
     text-align: center;
@@ -56,19 +98,6 @@
 
 }
 
-.docker__item {
-
-}
-.docker__title {
-  font-size: 20px;
-  margin-top: -.09rem;
-  transform: scale(0.5, 0.5);
-  transform-origin: center top;
-}
-
-.docker__item--active {
-  color: #1FA4FC;
-}
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
