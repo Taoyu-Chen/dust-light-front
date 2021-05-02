95bfcb0246b2a7a5d4fed266276a3f18fa8aad02
SevenSound <875693150@qq.com>
2021-05-03 03:20:39 +0800

    manage user joint debugging

--- src/views/administer/users/BusinessPeopleItem.vue
+++ src/views/administer/users/BusinessPeopleItem.vue
@@ -1,16 +1,85 @@
 <template>
   <van-swipe-cell>
-    <van-cell :border="true" title="bp username" value="phone" >
-      <van-button type="danger" size="mini" round>Delete</van-button>
-      <van-button type="warning" size="mini" round>Lock</van-button>
-    </van-cell>
+    <div
+      v-for="item in list"
+      :key="item._id"
+    >
+      <van-cell :border="true" :title="item.username">
+        <van-button type="danger" size="mini" @click="onDeleteUser(item._id)" round>Delete</van-button>&nbsp;
+        <van-button v-if="item.isLock===false" type="warning" size="mini" @click="onLockUser(item._id)" round>Lock</van-button>
+        <van-button v-if="item.isLock===true" type="success" size="mini" @click="onUnlockUser(item._id)" round>Unlock</van-button>
+      </van-cell>
+    </div>
   </van-swipe-cell>
 </template>
 
 <script>
+import { reactive, toRefs, watchEffect } from 'vue'
+import { get, del } from '../../../utils/request'
+import { useRouter } from 'vue-router'
+import { Notify } from 'vant'
+// Logic related to the business people user list
+const useBPListEffect = () => {
+  const content = reactive({ list: [] })
+  const getContentData = async () => {
+    const result = await get('/api/users/bp')
+    if (result?.errno === 0) {
+      content.list = result.data
+    }
+    console.log(result.data)
+  }
+
+  watchEffect(() => { getContentData() })
+  const { list } = toRefs(content)
+  return { list }
+}
+
+// logic about manage user
+const useManageUserEffect = () => {
+  const router = useRouter()
+  const onDeleteUser = async (userId) => {
+    const result = await del(`/api/users/${userId}`)
+    console.log(result)
+    if (result?.errno === 0) {
+      router.go(0)
+      Notify({ type: 'success', message: 'You have successfully delete a user!' })
+    } else {
+      Notify({ type: 'danger', message: 'You did not successfully delete a user' })
+    }
+  }
+  const onLockUser = async (userId) => {
+    const result = await get(`/api/users/lock/${userId}`)
+    console.log(result)
+    if (result?.errno === 0) {
+      router.go(0)
+      Notify({ type: 'success', message: 'You have successfully lock a user!' })
+    } else {
+      Notify({ type: 'danger', message: 'You did not successfully lock a user' })
+    }
+  }
+  const onUnlockUser = async (userId) => {
+    const result = await get(`/api/users/unlock/${userId}`)
+    console.log(result)
+    if (result?.errno === 0) {
+      router.go(0)
+      Notify({ type: 'success', message: 'You have successfully unlock a user!' })
+    } else {
+      Notify({ type: 'danger', message: 'You did not successfully unlock a user' })
+    }
+  }
+  return { onDeleteUser, onUnlockUser, onLockUser }
+}
+
 export default {
   setup () {
-
+    const { list } = useBPListEffect()
+    const { onDeleteUser, onUnlockUser, onLockUser } = useManageUserEffect()
+    return {
+      list,
+      onDeleteUser,
+      onUnlockUser,
+      onLockUser
+    }
   }
 }
 </script>
--- src/views/administer/users/index.vue
+++ src/views/administer/users/index.vue
@@ -10,6 +10,7 @@
         <freelaner-designer-list />
       </van-tab>
     </van-tabs>
+    <van-button class="logout-btn" type="primary" size="large" @click="onLogout" round>Logout</van-button>
   </div>
   <tabbar />
 </template>
@@ -18,8 +19,25 @@
 import Tabbar from '../Tabbar.vue'
 import { ref } from 'vue'
 import Nav from './Nav.vue'
+import { get } from '../../../utils/request'
 import BusinessPeopleList from './BusinessPeopleList.vue'
 import FreelanerDesignerList from './FreelancerDesignerList.vue'
+import { Notify } from 'vant'
+import { useRouter } from 'vue-router'
+
+const useLogoutEffect = () => {
+  const router = useRouter()
+  const onLogout = async () => {
+    const result = await get('/api/users/logout')
+    console.log(result)
+    if (result?.errno === 0) {
+      Notify({ type: 'danger', message: 'You have successfully logged out!' })
+      localStorage.clear()
+      router.push({ name: 'Login' })
+    }
+  }
+  return { onLogout }
+}
 
 export default {
   components: {
@@ -31,25 +49,31 @@ export default {
   name: 'ManageUsers',
   setup () {
     const activeName = ref('bp')
-    return { activeName }
+    const { onLogout } = useLogoutEffect()
+    return { activeName, onLogout }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../../../style/variables.scss';
-.gap {
-  margin: 0 0rem;
-  height: .02rem;
-  background: $content-bgcolor;
-}
-.wrapper {
-  overflow-y: auto;
-  position: absolute;
-  left: 0;
-  top: .5rem;
-  bottom: .5rem;
-  right: 0;
-  background: $wrapper-fontcolor;
-}
+  .gap {
+    margin: 0 0rem;
+    height: .02rem;
+    background: $content-bgcolor;
+  }
+  .wrapper {
+    overflow-y: auto;
+    position: absolute;
+    left: 0;
+    top: .5rem;
+    bottom: .5rem;
+    right: 0;
+    background: $wrapper-fontcolor;
+  }
+  .logout-btn {
+    margin-top: .2rem;
+    margin-left: 2%;
+    width: 96%;
+  }
 </style>
